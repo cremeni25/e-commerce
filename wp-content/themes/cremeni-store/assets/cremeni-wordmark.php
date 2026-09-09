64e494cd16611004534766ff42e600f5f411f780
cremeni25 <andersonnavarro.si@gmail.com>
2026-09-09 14:10:17 -0300

    fix: criar wordmark vetorial resiliente da CREMENI

---
+++ wp-content/themes/cremeni-store/assets/cremeni-wordmark.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Wordmark vetorial resiliente da interface CREMENI.
+ *
+ * @package CremeniStore
+ */
+if (! defined('ABSPATH')) { exit; }
+?>
+<span class="cremeni-wordmark" aria-label="CREMENI">
+    <span class="cremeni-wordmark__mark" aria-hidden="true"><i></i><b></b></span>
+    <span class="cremeni-wordmark__name">CREMENI</span>
+</span>
